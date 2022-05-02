e00c2ca6aa0b1c2f07f2e862dd4eb354ac096094
Alessandro Solbiati <lessandro@fb.com>
2022-05-02 01:46:21 +0100

    [client] hide nodes uncurated nodes (hack)

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,5 @@
 import Sigma from "sigma";
+import { NodeDisplayData } from "sigma/types";
 import * as jQuery from "jquery";
 import * as gexf from 'graphology-gexf';
 import GraphologyGraph from 'graphology';
@@ -9,13 +10,25 @@ jQuery.ajax( {
 		const graphData = res;
 		const graph = gexf.parse(GraphologyGraph, graphData);
 		const container = document.getElementById("sigma-container") as HTMLElement;
+
 		const renderer = new Sigma(graph, container);
 		renderer.on("clickNode", ({ node }) => {
 			const attr = graph.getNodeAttributes(node);
-			console.log(attr['fullpath']);
+			const fullpath = attr['fullpath']; 
+			console.log( "fullpath copied to clipboard", fullpath );
+			navigator.clipboard.writeText(fullpath);
+		});
+
+		renderer.setSetting("nodeReducer", (node, data) => {
+			const res: Partial<NodeDisplayData> = { ...data };
+			if (data.title.includes("GMT")){
+				res.hidden = true;
+			}
+			return res;
 		});
 		console.log(graph.order);
 		console.log(graph.size);
 		console.log("\n".repeat(50));
+
 	}
 });
