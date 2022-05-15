62fb4ecf0531fc82f909e56aa882fe8ffe1104fc
Alessandro Solbiati <lessandro@fb.com>
2022-05-15 17:50:08 +0100

    [note][multigraph] markdown multigraph path

--- src/server/note.ts
+++ src/server/note.ts
@@ -5,7 +5,6 @@ import { Graph, GraphNode, GraphBuilder } from "./graph.js";
 import { Event } from "./event.js";
 
 abstract class Note extends GraphNode {
-	public readonly PATH: string = GraphBuilder.CURRENT_GRAPH + constants.DATA.NOTE_PATH;
 	public mdfile: string;
 
 	additionalSaveValues() {
@@ -16,7 +15,7 @@ abstract class Note extends GraphNode {
 		}
 	}
 
-	constructor(){
+	constructor(public readonly PATH: string){
 		super();
 		this.mdfile = this.PATH + this.uuid + ".md";
 	}
@@ -26,25 +25,26 @@ abstract class Note extends GraphNode {
 export class CuratedNote extends Note {
 	public nodeType: string = constants.NODE_TYPES.CURATED_NOTE;
 	constructor(
+		PATH: string,
 		public parentUUID: string,
 		public title: string
 	){
-		super();
+		super(PATH);
 	}
 }
 
 export class UncuratedNote extends Note {
 	public nodeType: string = constants.NODE_TYPES.UNCURATED_NOTE;
-	constructor(){
-		super();
+	constructor(PATH: string){
+		super(PATH);
 		this.title = this.uuid;
 	}
 }
 
 export class Person extends Note {
 	public nodeType: string = constants.NODE_TYPES.PERSON;
-	constructor(personName){
-		super();
+	constructor(PATH: string, personName: string){
+		super(PATH);
 		this.title = personName;
 	}
 }
@@ -52,7 +52,7 @@ export class Person extends Note {
 export abstract class NoteBuilder {
 	public static readonly NOTE_FOOTER = "\n".repeat(100);
 	static createUncuratedNote(graph: Graph){
-		const note: Note = new UncuratedNote();
+		const note: Note = new UncuratedNote(graph.graph_path);
 		fs.writeFileSync(note.mdfile, NoteBuilder.NOTE_FOOTER+note.uuid);
 		graph.add(note);
 		GraphBuilder.save(graph);
@@ -73,7 +73,7 @@ export abstract class NoteBuilder {
 		parentNoteUUID: string
 	){
 		title = title ? title : "Untitled";
-		const note: CuratedNote = new CuratedNote(parentNoteUUID, title);
+		const note: CuratedNote = new CuratedNote(graph.graph_path, parentNoteUUID, title);
 		fs.writeFileSync(note.mdfile, "# "+title+NoteBuilder.NOTE_FOOTER+note.uuid);
 		graph.add(note);
 		if (parentNoteUUID){
@@ -87,7 +87,7 @@ export abstract class NoteBuilder {
 		graph: Graph,
 		personName: string,
 	){
-		const person: Person = new Person(personName	);
+		const person: Person = new Person(graph.graph_path, personName);
 		fs.writeFileSync(person.mdfile, "# "+personName+NoteBuilder.NOTE_FOOTER+person.uuid);
 		graph.add(person);
 		GraphBuilder.save(graph);
